98aa45d06fa928c8857b6956f888d3b02262a8f6
Anthony Leon <acidknights89@gmail.com>
2020-09-16 04:34:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,5 +8,6 @@ def key_for_min_value(name_hash)
   min_value = nil
   
   name_hash.each do |key, value|
-    if min_value = nil || value < key
+    if min_value = nil || value < min_value
+      
 end
