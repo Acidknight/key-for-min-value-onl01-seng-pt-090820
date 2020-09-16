b2fd5be53c7d649a7f1b49632219d88a050c0d30
Anthony Leon <acidknights89@gmail.com>
2020-09-16 04:37:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,14 +4,5 @@ name_hash = {:blake => 500, :ashley => 2, :adam => 1}
 name_hash = {:blake => 10, :ashley => 50, :adam => 17}
 name_hash = {}
 def key_for_min_value(name_hash)
-  min_key = nil
-  min_value = nil
-  
-  name_hash.each do |key, value|
-    if min_value == nil || value < min_value
-      min_value = key
-      min_key = value
-    end
-  end
-  min_key
+
 end
