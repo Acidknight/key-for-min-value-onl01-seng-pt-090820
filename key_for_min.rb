15dbd1097f5169bab77b67cd95c1931cb8f0a4e9
Anthony Leon <acidknights89@gmail.com>
2020-09-16 04:38:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,5 +4,5 @@ name_hash = {:blake => 500, :ashley => 2, :adam => 1}
 name_hash = {:blake => 10, :ashley => 50, :adam => 17}
 name_hash = {}
 def key_for_min_value(name_hash)
-  lowest =
+  lowest = name_hash.reduce ||
 end
